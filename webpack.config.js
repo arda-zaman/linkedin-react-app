b6fdf594426f3fd97522c70f358b950242453a03
arda-zaman <ardazamanla@gmail.com>
2021-12-18 12:37:30 +0300

    chore(scss): Scss supported

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,7 @@ module.exports = {
                 }
             },
             {
-                test: /\.css$/,
+                test: /\.scss$/,
                 use: ['style-loader', 'css-loader', 'sass-loader']
             }
         ]
