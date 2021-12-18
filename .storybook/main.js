d0a4d172b348ebd6faea907b30a9841ff822aac2
arda-zaman <ardazamanla@gmail.com>
2021-12-18 11:31:38 +0300

    chore(storybook): initialized

---
+++ .storybook/main.js
@@ -0,0 +1,11 @@
+module.exports = {
+  "stories": [
+    "../src/fields/**/*.stories.mdx",
+    "../src/fields/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials"
+  ],
+  "framework": "@storybook/react"
+}
---
+++ src/fields/button/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Button = () => {
+
+    return (
+        <button>click me</button>
+    )
+};
+
+Button.propTypes = {}
+
+export default Button;
