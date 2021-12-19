ff58011aa7f64e07012bc3242b19f1a63e16c149
arda-zaman <ardazamanla@gmail.com>
2021-12-19 14:47:13 +0300

    feat(HomePage): Create post card implemented

--- src/assets/files/index.js
+++ src/assets/files/index.js
@@ -3,3 +3,7 @@ export { default as LogoSmall } from './logo-small.svg';
 export { default as ProfileImage } from './profile-image.jpg';
 
 // ICONS
+export { default as PhotoSVG } from './icons/photo.svg';
+export { default as VideoSVG } from './icons/video.svg';
+export { default as EventSVG } from './icons/event.svg';
+export { default as WriteArticleSVG } from './icons/write-article.svg';
--- src/pages/home/page-content.js
+++ src/pages/home/page-content.js
@@ -1,10 +1,41 @@
 import React from 'react';
+import { Button, Card, Divider } from '../../fields';
+import { PhotoSVG, VideoSVG, WriteArticleSVG, EventSVG, ProfileImage } from '../../assets/files'
 
 const PageContent = () => {
 
     return (
         <div className="home-page-content">
-            Page Content
+            <Card id="post-comment" block>
+                <div className="top d-flex">
+                    <img src={ProfileImage} className="radius-50" width={50} height={50} />
+                    <Button type="secondary" shape="circle" block>Start a post</Button>
+                </div>
+
+                <div className="post-kinds d-flex justify-between w100">
+                    <Button type="link" block>
+                        <PhotoSVG fill="rgb(112, 181, 249)" />
+                        <span>Photo</span>
+                    </Button>
+
+                    <Button type="link" block>
+                        <VideoSVG fill="rgb(127, 193, 94)" />
+                        <span>Video</span>
+                    </Button>
+
+                    <Button type="link" block>
+                        <EventSVG fill="rgb(231, 163, 62)" />
+                        <span>Event</span>
+                    </Button>
+
+                    <Button type="link" block>
+                        <WriteArticleSVG fill="rgb(252, 146, 149)" />
+                        <span>Write article</span>
+                    </Button>
+                </div>
+            </Card>
+
+            <Divider />
         </div>
     )
 };
