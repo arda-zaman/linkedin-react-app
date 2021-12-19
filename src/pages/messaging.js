d6216dfa03f4decd24d6cedd852200afcf93daf3
arda-zaman <ardazamanla@gmail.com>
2021-12-19 12:47:58 +0300

    feat(Routes): Empty pages created

---
+++ src/pages/messaging.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Component = () => <div>Will be implemented soon</div>;
+
+export default Component;
--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './containers/app';
-import HelloWorld from './pages/hello-world';
+import Home from './pages/home';
+import MyNetworks from './pages/mynetworks';
+import Jobs from './pages/jobs';
+import Messaging from './pages/messaging';
+import Notifications from './pages/notifications';
 
 const RoutesComp = () => (
     <BrowserRouter>
         <Routes>
-            <Route path="*" element={<App />}>
-                <Route path="hello-world" element={<HelloWorld />} />
+            <Route path="/" element={<App />}>
+                <Route path="/mynetwork" element={<MyNetworks />} />
+                <Route path="/jobs" element={<Jobs />} />
+                <Route path="/messaging" element={<Messaging />} />
+                <Route path="/notifications" element={<Notifications />} />
+                <Route path="/" element={<Home />} />
             </Route>
             <Route path="*" element={<div>not found</div>} />
         </Routes>
